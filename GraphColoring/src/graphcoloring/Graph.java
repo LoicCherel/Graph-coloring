b84e00e6f9e15b18769d50e5401c573125c86eaa
Thomas Raynaud <raynaud.thomas@gmail.com>
2016-09-14 13:00:20 +0200

    Netbeans project added
    The netbeans project has been created and added to the git repository.

---
+++ GraphColoring/src/graphcoloring/Graph.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphcoloring;
+
+/**
+ *
+ * @author p1401687
+ */
+public class Graph {
+    
+}
---
+++ GraphColoring/src/graphcoloring/GraphColoring.java
@@ -0,0 +1,16 @@
+package graphcoloring;
+
+/**
+ *
+ * @author p1401687
+ */
+public class GraphColoring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
