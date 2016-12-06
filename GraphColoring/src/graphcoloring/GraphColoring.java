5a644b0a5ee3c3efd797a9c663790a95c892ea0b
Thomas Raynaud <raynaud.thomas@gmail.com>
2016-12-06 12:04:00 +0100

    Modification du fichier main pour générer des statistiques sur les paramètres de l'ARS

--- GraphColoring/src/graphcoloring/GraphColoring.java
+++ GraphColoring/src/graphcoloring/GraphColoring.java
@@ -1,7 +1,13 @@
 package graphcoloring;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 
 /**
@@ -13,32 +19,92 @@ public class GraphColoring {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        //S = Nombre de sommets
+        //I = Nombre d'itérations avant de prendre le graphe avec le moins de couleurs
+        //C = Nombre d'appels récursifs pour adapter les voisins
+        //T = Température maximum
+        
+        //POUR I
+        String outputFile_I = "dataARS_I.csv";
+        String string = "";
         GraphARS graphARS = new GraphARS();
-        for(int nbSommets = 10; nbSommets < 50; nbSommets += 5){
-            Graph graph = new Graph(nbSommets);
-            for(int nbIterationsBeforeUsingGraphMin = 30; nbIterationsBeforeUsingGraphMin > 3; nbIterationsBeforeUsingGraphMin -= 5){
-                for(int changementVoisins = 50; changementVoisins > 3; changementVoisins -= 5){
-                    for(int temp = 2000; temp < 10000; temp += 200){
-                        int[] nbColorsObtained = new int[5];
-                        for(int nbTests = 0; nbTests < 5; nbTests++){
-                            graphARS = GraphARS.toGraphARS(graph);
-                            graphARS.applySimulatedAnnealingAlgorithm(false, temp, nbIterationsBeforeUsingGraphMin, changementVoisins);
-                            nbColorsObtained[nbTests] = graphARS.getNumberOfColors();
-                            
-                        }
-                        SummaryStatistics statsNbColorsObtained = new SummaryStatistics();
-                        for (int val : nbColorsObtained) {
-                            statsNbColorsObtained.addValue(val);
-                        }
-                        BigDecimal mean = new BigDecimal(statsNbColorsObtained.getMean());
-                        mean = mean.setScale(3, RoundingMode.HALF_UP);
-                        System.out.println("NBsommets = " + nbSommets + ", I = " + nbIterationsBeforeUsingGraphMin +
-                                    ", C = " + changementVoisins + ", Temperature = " + temp + ", NBCOULEURS = " + mean.toString());
+        for(int S = 10; S < 50; S += 5){
+            System.out.println(S);
+            Graph graph = new Graph(S);
+            for(int I = 30; I > 3; I -= 5){
+                System.out.println("  " + I);
+                double[] tab_C = new double[10];
+                int compteur_C = 0;
+                for(int C = 50; C > 3; C -= 5){
+                    double[] tab_T = new double[20];
+                    int compteur_T = 0;
+                    for(int T = 2000; T < 10000; T += 400){
+                        graphARS = GraphARS.toGraphARS(graph);
+                        graphARS.applySimulatedAnnealingAlgorithm(false, T, I, C);
+                        tab_T[compteur_T] = graphARS.getNumberOfColors();
+                        compteur_T++;
                     }
+                    SummaryStatistics statsT = new SummaryStatistics();
+                    for (double val : tab_T) {
+                        statsT.addValue(val);
+                    }
+                    BigDecimal meanT = new BigDecimal(statsT.getMean());
+                    meanT = meanT.setScale(3, RoundingMode.HALF_UP);
+                    tab_C[compteur_C] = meanT.doubleValue();
+                    compteur_C++;
                 }
+                SummaryStatistics statsI = new SummaryStatistics();
+                for (double val : tab_C) {
+                    statsI.addValue(val);
+                }
+                BigDecimal meanC = new BigDecimal(statsI.getMean());
+                meanC = meanC.setScale(3, RoundingMode.HALF_UP);
+                string += S + "; " + I + "; " + meanC + ";\n";
             }
-            
         }
+        Files.write(Paths.get(outputFile_I), (string).getBytes(), StandardOpenOption.CREATE);
+           /*System.out.println("NBsommets = " + S + ", I = " + I +
+                                    ", C = " + C + ", Temperature = " + T + ", NBCOULEURS = " + mean.toString());*/
+        //POUR T
+        /*String outputFile_T = "dataARS_T.csv";
+        string = "";
+        for(int S = 10; S < 50; S += 5){
+            System.out.println(S);
+            Graph graph = new Graph(S);
+            for(int T = 2000; T < 10000; T += 400){
+                System.out.println("  " + T);
+                double[] tab_C = new double[10];
+                int compteur_C = 0;
+                for(int C = 50; C > 3; C -= 5){
+                    double[] tab_I = new double[6];
+                    int compteur_I = 0;
+                    for(int I = 30; I > 3; I -= 5){
+                        graphARS = GraphARS.toGraphARS(graph);
+                        graphARS.applySimulatedAnnealingAlgorithm(false, T, I, C);
+                        tab_I[compteur_I] = graphARS.getNumberOfColors();
+                        compteur_I++;
+                    }
+                    SummaryStatistics statsT = new SummaryStatistics();
+                    for (double val : tab_I) {
+                        statsT.addValue(val);
+                    }
+                    BigDecimal meanT = new BigDecimal(statsT.getMean());
+                    meanT = meanT.setScale(3, RoundingMode.HALF_UP);
+                    tab_C[compteur_C] = meanT.doubleValue();
+                    compteur_C++;
+                }
+                SummaryStatistics statsI = new SummaryStatistics();
+                for (double val : tab_C) {
+                    statsI.addValue(val);
+                }
+                BigDecimal meanC = new BigDecimal(statsI.getMean());
+                meanC = meanC.setScale(3, RoundingMode.HALF_UP);
+                string += S + "; " + T + "; " + meanC + ";\n";
+            }
+        }
+        Files.write(Paths.get(outputFile_T), (string).getBytes(), StandardOpenOption.CREATE);*/
+           /*System.out.println("NBsommets = " + S + ", I = " + I +
+                                    ", C = " + C + ", Temperature = " + T + ", NBCOULEURS = " + mean.toString());*/
     }
 }
